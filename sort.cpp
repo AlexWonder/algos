08ea75207b9ec74f925f6fbe1af1cc42e2670bd3
AlexWonder <rich.stp.p@gmail.com>
2017-02-15 10:32:25 -0400

    Changed it from search to sort

--- sort.cpp
+++ sort.cpp
@@ -12,14 +12,15 @@ using namespace std;
 \param [in] data The data set that will be searched
 \returns location of key if found or -1 if not found
 */
-int linearSearch(auto data, auto key);//prototype
-
+//int linearSearch(auto data, auto key);//prototype
+void bubbleSort(auto& Data);//prototype
 
 int main()
 {
   vector<string> inputs;
-  string search_key, input;
-  int result;
+  //string search_key 
+  string input;
+  //int result;
 
    cout<<"Welcome to \"search it\". We first need some input data."<<endl;
    cout<<"We'll assume the inputs do not have any spaces."<<endl<<endl;
@@ -40,12 +41,17 @@ int main()
       cout<<endl<<"No input received, quiting..."<<endl<<endl;
        exit(1);//nothing to do but quit program
   }
+  
+  bubbleSort(inputs);
+  
+  for (int i = 0; i<inputs.size(); ++i)
+	cout << inputs[i] << endl;
  
-   cout<<endl<<"To end input type the #-character (followed by Enter)"<<endl<<endl;
-  cout<<"Enter a value to search for: ";
+   //cout<<endl<<"To end input type the #-character (followed by Enter)"<<endl<<endl;
+  //cout<<"Enter a value to search for: ";
 
 
-   cin>>search_key;
+   /*cin>>search_key;
  
     while(search_key != "#")//perform searches until sentinel entered
     {
@@ -61,17 +67,25 @@ int main()
 
         cout<<endl<<endl<<"Enter a value to search for: ";
         cin>>search_key; 
-    }
+    }*/
 
    cout<<endl<<"Program \"search it\" is now finished."<<endl<<endl;
 
     return 0;
 }
 
-int linearSearch(auto data, auto key)
+void bubbleSort(auto& Data)
+{
+	for (int i = 0; i<Data.size(); ++i)
+		for (int j = 0; j<Data.size()-1; ++j)
+			if (Data[j]>Data[j+1])
+				swap(Data[j], Data[j+1]);
+}
+
+/*int linearSearch(auto data, auto key)
 {
 	for (int i=0; i<data.size(); ++i)
 		if (data[i]==key)
 			return i;
 	return -1; //not found
-}
+}*/
